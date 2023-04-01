1a2c730290e04a16002bd82772a656cdfde50e6b
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-04-02 01:20:27 +0300

    Did first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
 
+let intervalId = null;
+let clicked = false;
+
+startBtnEl.addEventListener('click', handleStartBtnClick);
+stopBtnEl.addEventListener('click', handleStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function handleStartBtnClick(evt) {
+  clicked = true;
+
+  if (clicked) {
+    evt.target.disabled = true;
+    stopBtnEl.disabled = false;
+  }
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handleStopBtnClick(evt) {
+  clicked = true;
+
+  if (clicked) {
+    evt.target.disabled = true;
+    startBtnEl.disabled = false;
+  }
+  clearInterval(intervalId);
+}
