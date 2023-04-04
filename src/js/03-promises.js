a4b8b5013b6c55af514c6c446103632ef87d621b
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-04-05 00:01:56 +0300

    Did third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+const delayInputEl = formEl.elements.delay;
+const stepInputEl = formEl.elements.step;
+const amountInputEl = formEl.elements.amount;
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleCreatePromisesSubmit(evt) {
+  evt.preventDefault();
+
+  let delay = Number(delayInputEl.value);
+  const step = Number(stepInputEl.value);
+  const amount = Number(amountInputEl.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    const position = i + 1;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
+
+formEl.addEventListener('submit', handleCreatePromisesSubmit);
